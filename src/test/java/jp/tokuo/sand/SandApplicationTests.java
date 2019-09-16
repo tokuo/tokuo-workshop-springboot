7dc86f77574c81fca74a3fb18fd01c339763dd96
tokuo <@yahoo.co.jp>
2019-09-16 09:23:33 +0900

    change junit5

--- src/test/java/jp/tokuo/sand/SandApplicationTests.java
+++ src/test/java/jp/tokuo/sand/SandApplicationTests.java
@@ -1,16 +1,17 @@
 package jp.tokuo.sand;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SandApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("JUnit5 TEST in springboot");
 	}
 
 }
