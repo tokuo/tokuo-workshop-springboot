09fe7dd169d98253d644991f0e67c964c889e36b
tokuo <@yahoo.co.jp>
2019-11-25 00:45:09 +0900

    add micrometor

--- tokuo-sand-data-sample/src/main/java/jp/tokuo/sand/controller/DataController.java
+++ tokuo-sand-data-sample/src/main/java/jp/tokuo/sand/controller/DataController.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -18,13 +21,16 @@ import lombok.extern.slf4j.Slf4j;
 public class DataController {
 
   private final ObjectMapper objectMapper;
+  private final Counter myCounter;
 
-  public DataController(ObjectMapper objectMapper){
+  public DataController(ObjectMapper objectMapper, MeterRegistry meterRegistry){
     this.objectMapper = objectMapper;
+    this.myCounter = meterRegistry.counter("jp.tokuo.sand", "myTagName1", "myTagValue1", "myTagName2", "myTagValue2");
   }
 
 
   @GetMapping(path = "/test/{anyWord}")
+  @Timed
   public ResponseEntity<String> test(@PathVariable String anyWord) throws JsonProcessingException {
     SampleData res = new SampleData();
     res.setCandidate(anyWord);
@@ -35,6 +41,7 @@ public class DataController {
       ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("セキュリティ的に問題なければ、5W1Hで記述");
     } finally {
       log.debug("デバッグ時のみ出力");
+      myCounter.increment();
     }
     return ResponseEntity.ok().body(objectMapper.writeValueAsString(res));
   }
