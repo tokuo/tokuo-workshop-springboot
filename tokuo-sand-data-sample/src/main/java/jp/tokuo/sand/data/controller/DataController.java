edb67784c5453fda49d8d53110afdf2a35d8cabb
tokuo <@yahoo.co.jp>
2019-12-25 04:22:26 +0900

    add Date and Time api

--- tokuo-sand-data-sample/src/main/java/jp/tokuo/sand/data/controller/DataController.java
+++ tokuo-sand-data-sample/src/main/java/jp/tokuo/sand/data/controller/DataController.java
@@ -4,37 +4,49 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jp.tokuo.sand.data.model.SampleData;
+import jp.tokuo.sand.data.service.DateTimeService;
 import jp.tokuo.sand.data.service.ParallelService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1")
 @Slf4j
 public class DataController {
 
   private final ParallelService parallelService;
+  private final DateTimeService dateTimeService;
   private final ObjectMapper objectMapper;
   private final Counter myCounter; // Counter, Gauge, Histogram, Summary
 
-  public DataController(ParallelService parallelService, ObjectMapper objectMapper, MeterRegistry meterRegistry){
+  public DataController(
+      ParallelService parallelService,
+      DateTimeService dateTimeService,
+      ObjectMapper objectMapper,
+      MeterRegistry meterRegistry
+  ){
     this.parallelService = parallelService;
+    this.dateTimeService = dateTimeService;
     this.objectMapper = objectMapper;
     this.myCounter = meterRegistry.counter("jp.tokuo.sand", "myTagName1", "myTagValue1", "myTagName2", "myTagValue2");
   }
 
 
-  @GetMapping(path = "/test/{anyWord}")
+  @GetMapping(value = "/get/{anyWord}")
   @Timed
-  public ResponseEntity<String> test(@PathVariable String anyWord) throws JsonProcessingException {
+  public ResponseEntity<String> getTest(@PathVariable String anyWord) throws JsonProcessingException {
     SampleData res = new SampleData();
     res.setCandidate(anyWord);
     try {
@@ -46,5 +58,14 @@ public class DataController {
       myCounter.increment();
     }
     return ResponseEntity.ok().body(objectMapper.writeValueAsString(res));
+  }
+
+  @PostMapping(value = "/post")
+  public ResponseEntity<String> postTest(
+      @RequestParam(name = "param01", required = false) String param01,
+      @RequestParam(name = "param02", required = false) String param02
+  ) {
+    dateTimeService.showDateTime();
+    return ResponseEntity.ok().body(String.format("{\"param\": \"param\"}"));
   }
 }
---
+++ tokuo-sand-data-sample/src/main/java/jp/tokuo/sand/data/service/DateTimeService.java
@@ -0,0 +1,62 @@
+package jp.tokuo.sand.data.service;
+
+import org.springframework.stereotype.Component;
+
+// java8からjava.utilではなく、java.timeにあるapi群を利用する方が良い
+import java.sql.Timestamp;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.Period;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class DateTimeService {
+
+  public void showDateTime(){
+    // LocalDateTime, LocalDate, LocalTime
+    LocalDateTime nowDateTime = LocalDateTime.now(Clock.systemDefaultZone());
+    LocalDate nowDate = nowDateTime.toLocalDate(); // = LocalDate.now() or LocalDate.now(Clock.systemDefaultZone())
+    LocalTime nowTime = nowDateTime.toLocalTime(); // = LocalTime.now() or LocalTime.now(Clock.systemDefaultZone())
+    log.info(String.format("{LocalDateTime: %s, LocalDate: %s, LocalTime %s}",
+        nowDateTime, nowDate, nowTime));
+    nowDateTime = LocalDateTime.of(nowDate, LocalTime.of(nowDateTime.getHour(), 0, 0, 0));
+    log.info(String.format("nowDateTime: %s", nowDateTime));
+
+    // java.sql.XXX
+    Timestamp timestamp = Timestamp.valueOf(nowDateTime);
+    log.info(String.format("nowDateTime for Sql: %s", timestamp));
+
+    // format
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime formattedNowDateTime = LocalDateTime.parse("2020/01/01 12:00:00", formatter);
+    log.info(String.format("formatted LocalDateTime: %s", formattedNowDateTime));
+
+    // compare
+    TemporalAmount temporalAmount = Period.ofDays(-3);
+    LocalDateTime minusDateTimeFromNow = nowDateTime.plus(temporalAmount); // = nowDateTime.plusDays(-3)
+    boolean flag = nowDateTime.isAfter(minusDateTimeFromNow); // flag == true  nowDateTime after timeBeforNowDateTime
+
+    // difference
+    long diffDay = ChronoUnit.DAYS.between(nowDateTime, minusDateTimeFromNow);
+    TemporalAmount duration = Duration.between(nowDateTime, minusDateTimeFromNow);
+    TemporalAmount period = Period.between(nowDateTime.toLocalDate(), minusDateTimeFromNow.toLocalDate());
+    log.info(String.format("{Difference in long: %s, Difference in duration: %s, Difference in period: %s}",
+        diffDay, duration, period));
+
+    // ISO 8601 準拠
+    OffsetDateTime offsetDateTime = OffsetDateTime.of(nowDateTime, ZoneOffset.UTC);
+    OffsetDateTime changedOffsetDataTime = offsetDateTime.plusDays(-3); // LocalDateTime型のplusと異なり、immutableなのでplus()しても元オブジェクトに変化なし
+    log.info(String.format("{OffsetDateTime based on ISO 8601: %s, changed OffsetDataTime: %s",
+        offsetDateTime, changedOffsetDataTime));
+  }
+}
